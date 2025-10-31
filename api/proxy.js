916e8b8f34beed61dca53803f7c47766c2ef77a4
WL0207 <1352158198@qq.com>
2025-10-31 09:57:17 +0800

    转发请求1

--- api/proxy.js
+++ api/proxy.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
 // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-    if (req.url.startsWith('/api/comment')) {
+    if (req.url.startsWith('/api/proxy/api/comment')) {
         target = 'https://walinecomments-gxccu74d8-chens-projects-7c4f92c5.vercel.app'
     }
     // 创建代理对象并转发请求
@@ -23,7 +23,7 @@ module.exports = (req, res) => {
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/backend`
             // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-            '^/api/comment': '/api/comment'
+            '^/api/proxy/': '/'
         }
     })(req, res)
 }
